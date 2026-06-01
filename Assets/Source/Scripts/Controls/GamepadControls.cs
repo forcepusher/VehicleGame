9654498aa23755ba02ff7151a198952f6b91fe14
forcepusher <bionitsoup@gmail.com>
2026-06-01 19:14:14 +0700

    Add placeholders

--- Assets/Source/Scripts/Controls/GamepadControls.cs
+++ Assets/Source/Scripts/Controls/GamepadControls.cs
@@ -1,7 +1,13 @@
 namespace Igrushka.VehicleGame
 {
-    public class GamepadControls
+    public class GamepadControls : IControls
     {
+        public float Throttle => throw new System.NotImplementedException();
 
+        public float Turn => throw new System.NotImplementedException();
+
+        public float Pitch => throw new System.NotImplementedException();
+
+        public float Yaw => throw new System.NotImplementedException();
     }
 }
--- Assets/Source/Scripts/Controls/IControls.cs
+++ Assets/Source/Scripts/Controls/IControls.cs
@@ -2,6 +2,9 @@ namespace Igrushka.VehicleGame
 {
     public interface IControls
     {
-
+        public float Throttle { get; }
+        public float Turn { get; }
+        public float Pitch { get; }
+        public float Yaw { get; }
     }
 }
