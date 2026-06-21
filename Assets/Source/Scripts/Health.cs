6b373d76c526b44b98614ef50e501e23a806ede3
forcepusher <bionitsoup@gmail.com>
2026-06-22 02:23:00 +0700

    Add health script

---
+++ Assets/Source/Scripts/Health.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace BananaParty.VehicleGame
+{
+    public class Health : MonoBehaviour
+    {
+        public int Value { get; private set; } = 100;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            int damage = 0;
+            Debug.Log(damage);
+        }
+    }
+}
