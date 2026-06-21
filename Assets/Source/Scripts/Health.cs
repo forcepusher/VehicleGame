b9088008a5a28d80d64ebc2b4088e02dd6f94be8
forcepusher <bionitsoup@gmail.com>
2026-06-22 02:34:02 +0700

    Adding health

--- Assets/Source/Scripts/Health.cs
+++ Assets/Source/Scripts/Health.cs
@@ -4,12 +4,16 @@ namespace BananaParty.VehicleGame
 {
     public class Health : MonoBehaviour
     {
+        private const float _collisionDamageMultiplier = 1f;
+
         public int Value { get; private set; } = 100;
 
         private void OnCollisionEnter(Collision collision)
         {
-            int damage = 0;
-            Debug.Log(damage);
+            int damage = Mathf.RoundToInt(collision.relativeVelocity.magnitude * _collisionDamageMultiplier);
+            Value -= damage;
+            Value = Mathf.Max(0, Value);
+            Debug.Log($"Took {damage} damage. Current health: {Value}");
         }
     }
 }
