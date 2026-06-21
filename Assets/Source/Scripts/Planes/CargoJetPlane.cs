4e7cca0257ff78bfa1b3261489c4c7b92280d153
forcepusher <bionitsoup@gmail.com>
2026-06-21 20:28:18 +0700

    Tweak controls

--- Assets/Source/Scripts/Planes/CargoJetPlane.cs
+++ Assets/Source/Scripts/Planes/CargoJetPlane.cs
@@ -17,7 +17,7 @@ namespace BananaParty.VehicleGame
         protected override Vector3 AngularAccelerationTaxi => new Vector3(0f, 0.5f, 0f); // pitch: yaw: roll
 
         protected override float AccelerationFight => 10f;
-        protected override Vector3 AngularAccelerationFlight => new Vector3(1f, 1f, 1f); // pitch: yaw: roll
+        protected override Vector3 AngularAccelerationFlight => new Vector3(0.5f, 0.5f, 0.5f); // pitch: yaw: roll
 
         // Linear drag per axis; z=forward direction should be lowest (streamlined jet).
         protected override Vector3 DragParked => new Vector3(0.0f, 0.0f, 0.0f);
@@ -26,7 +26,7 @@ namespace BananaParty.VehicleGame
         protected override Vector3 DragTaxi => new Vector3(0.05f, 0.05f, 0.005f);
         protected override Vector3 AngularDragTaxi => new Vector3(0.5f, 0.5f, 0.5f); // pitch: yaw: roll
 
-        protected override Vector3 DragFlight => new Vector3(0.3f, 0.3f, 0.005f);
+        protected override Vector3 DragFlight => new Vector3(0.5f, 0.75f, 0.005f);
         protected override Vector3 AngularDragFlight => new Vector3(3.0f, 3.0f, 3.0f); // pitch: yaw: roll
     }
 }
