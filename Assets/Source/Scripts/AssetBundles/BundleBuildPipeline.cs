d7962c7e24fdacd3f76b308d7965ff386d2ab51c
forcepusher <bionitsoup@gmail.com>
2026-06-22 15:32:47 +0700

    Asset bundles

--- Assets/Source/Scripts/AssetBundles/BundleBuildPipeline.cs
+++ Assets/Source/Scripts/AssetBundles/BundleBuildPipeline.cs
@@ -10,11 +10,14 @@ namespace BananaParty.VehicleGame
         [MenuItem("AssetBundles/Build")]
         public static void Build()
         {
+            List<AssetBundleBuild> assetBundleDefinitionList = new();
+
             for (int terrainIndexX = 0; terrainIndexX < 10; terrainIndexX += 1)
             {
                 for (int terrainIndexY = 0; terrainIndexY < 10; terrainIndexY += 1)
                 {
                     string assetBundleName = $"Terrain_{terrainIndexX}_{terrainIndexY}";
+
                     AssetBundleBuild assetBundleBuild = new();
                     assetBundleBuild.assetBundleName = $"Terrain_x0{terrainIndexX}_y0{terrainIndexY}";
                     assetBundleBuild.assetNames = new string[]
@@ -23,8 +26,18 @@ namespace BananaParty.VehicleGame
                         $"TerrainProject Bitmap Output 4096_x0{terrainIndexX}_y0{terrainIndexY}",
                         $"TerrainProject Mesh Output_x0{terrainIndexX}_y0{terrainIndexY}"
                     };
+
+                    assetBundleDefinitionList.Add(assetBundleBuild);
                 }
             }
+
+            BuildAssetBundlesParameters buildParameters = new()
+            {
+                outputPath = "",
+                options = BuildAssetBundleOptions.AssetBundleStripUnityVersion,
+                bundleDefinitions = assetBundleDefinitionList.ToArray()
+            };
+            var manifest = BuildPipeline.BuildAssetBundles(buildParameters);
         }
     }
 }
