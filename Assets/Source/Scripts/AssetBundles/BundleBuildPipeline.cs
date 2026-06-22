30c1f425656f009d86ea014954d4cfb5c3789c8c
forcepusher <bionitsoup@gmail.com>
2026-06-22 15:17:27 +0700

    Asset bundles

---
+++ Assets/Source/Scripts/AssetBundles/BundleBuildPipeline.cs
@@ -0,0 +1,22 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace BananaParty.VehicleGame
+{
+    public static class BundleBuildPipeline
+    {
+        [MenuItem("AssetBundles/Build")]
+        public static void Build()
+        {
+            for (int terrainIndexX = 0; terrainIndexX < 10; terrainIndexX += 1)
+            {
+                for (int terrainIndexY = 0; terrainIndexY < 10; terrainIndexY += 1)
+                {
+                    string assetBundleName = $"Terrain_{terrainIndexX}_{terrainIndexY}";
+                    AssetBundleBuild assetBundleBuild = new();
+                    assetBundleBuild.assetBundleName = $"Terrain_{terrainIndexX}_{terrainIndexY}";
+                }
+            }
+        }
+    }
+}
