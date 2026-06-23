b242202eb4c650096adec3cc7ed5f2d0411915f5
forcepusher <bionitsoup@gmail.com>
2026-06-24 01:28:43 +0700

    Improve mouse lock

--- Assets/Source/Scripts/MouseLock.cs
+++ Assets/Source/Scripts/MouseLock.cs
@@ -1,26 +1,61 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 namespace BananaParty.VehicleGame
 {
     public class MouseLock : MonoBehaviour
     {
+        private bool _wantsLock;
+
+        private void Awake()
+        {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            WebGLInput.stickyCursorLock = false;
+#endif
+        }
+
         private void Update()
         {
-#if !UNITY_WEBGL || UNITY_EDITOR
             if (Keyboard.current?.escapeKey.wasPressedThisFrame == true)
             {
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
+                UnlockCursor();
+                return;
             }
-#endif
 
-            if (Mouse.current?.leftButton.wasPressedThisFrame == true && !EventSystem.current.IsPointerOverGameObject())
+            if (Mouse.current?.leftButton.wasPressedThisFrame == true && !IsPointerOverUi())
+                LockCursor();
+        }
+
+        private void LockCursor()
+        {
+            _wantsLock = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        private void UnlockCursor()
+        {
+            _wantsLock = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        private bool IsPointerOverUi()
+        {
+            EventSystem eventSystem = EventSystem.current;
+            if (eventSystem == null || Mouse.current == null)
+                return false;
+
+            PointerEventData pointerEventData = new PointerEventData(eventSystem)
             {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
+                position = Mouse.current.position.ReadValue()
+            };
+
+            List<RaycastResult> raycastResults = new List<RaycastResult>();
+            eventSystem.RaycastAll(pointerEventData, raycastResults);
+            return raycastResults.Count > 0;
         }
     }
 }
