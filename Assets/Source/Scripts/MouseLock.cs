0b1b0329a6c177069facb2153a4bc646fdde30e7
forcepusher <bionitsoup@gmail.com>
2026-06-24 01:38:07 +0700

    Working mouse lock

--- Assets/Source/Scripts/MouseLock.cs
+++ Assets/Source/Scripts/MouseLock.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -7,21 +8,41 @@ namespace BananaParty.VehicleGame
 {
     public class MouseLock : MonoBehaviour
     {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        [DllImport("__Internal")]
+        private static extern void PointerLockTracker_Init();
+
+        [DllImport("__Internal")]
+        private static extern int PointerLockTracker_CanRequestLock();
+#endif
+
         private void Awake()
         {
 #if UNITY_WEBGL && !UNITY_EDITOR
             WebGLInput.stickyCursorLock = false;
+            PointerLockTracker_Init();
 #endif
         }
 
         private void Update()
         {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            if (!CanRequestLock())
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+#endif
+
 #if !UNITY_WEBGL || UNITY_EDITOR
             if (Keyboard.current?.escapeKey.wasPressedThisFrame == true)
                 Cursor.lockState = CursorLockMode.None;
 #endif
 
-            if (Mouse.current?.leftButton.wasPressedThisFrame == true && !IsPointerOverUi())
+            if (Mouse.current?.leftButton.wasPressedThisFrame == true
+                && !IsPointerOverUi()
+                && CanRequestLock()
+                && Cursor.lockState == CursorLockMode.None)
             {
                 Cursor.lockState = CursorLockMode.Locked;
                 Cursor.visible = false;
@@ -29,6 +50,15 @@ namespace BananaParty.VehicleGame
 
             if (Cursor.lockState != CursorLockMode.Locked)
                 Cursor.visible = true;
+        }
+
+        private bool CanRequestLock()
+        {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            return PointerLockTracker_CanRequestLock() != 0;
+#else
+            return true;
+#endif
         }
 
         private bool IsPointerOverUi()
