7cb0e378d64a4b0b0373f47eab4b44521eb10d90
forcepusher <bionitsoup@gmail.com>
2026-06-24 13:49:05 +0700

    Script gravel sound

--- Assets/Source/Scripts/Planes/JetPlaneSounds.cs
+++ Assets/Source/Scripts/Planes/JetPlaneSounds.cs
@@ -10,6 +10,8 @@ namespace BananaParty.VehicleGame
         private AudioSource _engineAudioSource;
         [SerializeField]
         private AudioSource _airAudioSource;
+        [SerializeField]
+        private AudioSource _gravelAudioSource;
 
         [SerializeField]
         private AnimationCurve _engineVolumeCurve;
@@ -19,15 +21,41 @@ namespace BananaParty.VehicleGame
         private AnimationCurve _airVolumeCurve;
         [SerializeField]
         private AnimationCurve _airPitchCurve;
+        [SerializeField]
+        private AnimationCurve _gravelVolumeCurve;
+        [SerializeField]
+        private AnimationCurve _gravelPitchCurve;
 
         public bool IsEngineRunning { get; private set; }
+        private bool _gravelSoundPlaying;
 
-        public void UpdateVelocity(float velocity)
+
+        public void UpdateVelocity(float velocity, bool grounded)
         {
             _engineAudioSource.volume = _engineVolumeCurve.Evaluate(velocity);
             _engineAudioSource.pitch = _enginePitchCurve.Evaluate(velocity);
             _airAudioSource.volume = _airVolumeCurve.Evaluate(velocity);
             _airAudioSource.pitch = _airPitchCurve.Evaluate(velocity);
+            _gravelAudioSource.volume = _gravelVolumeCurve.Evaluate(velocity);
+            _gravelAudioSource.pitch = _gravelPitchCurve.Evaluate(velocity);
+
+            if (grounded)
+            {
+                if (!_gravelSoundPlaying)
+                {
+                    _gravelAudioSource.loop = true;
+                    _gravelAudioSource.Play();
+                    _gravelSoundPlaying = true;
+                }
+            }
+            else
+            {
+                if (_gravelSoundPlaying)
+                {
+                    _gravelAudioSource.loop = false;
+                    _gravelSoundPlaying = false;
+                }
+            }
         }
 
         public void StartEngine()
