1c7426c6d67bd1a980b43761647041d2a851c70c
forcepusher <bionitsoup@gmail.com>
2026-06-26 05:41:23 +0700

    Refactor

--- Assets/Source/Scripts/UI/LocalPlayerUserInterface.cs
+++ Assets/Source/Scripts/UI/LocalPlayerUserInterface.cs
@@ -5,16 +5,54 @@ namespace BananaParty.VehicleGame
 {
     public class LocalPlayerUserInterface : MonoBehaviour, IUserInterface
     {
+        private const float AutoSpawnTimeSeconds = 10f;
+
+        [SerializeField]
+        private Text _autoSpawnText;
         [SerializeField]
         private Image _healthBarFill;
         [SerializeField]
         private Text _healthText;
 
+        private float _timeRemainingToAutoSpawn = AutoSpawnTimeSeconds;
+        private string _initialRespawnText;
+
         private IVehicle _controlledVehicle;
 
         public bool IsSpawnRequested { get; private set; }
 
-        public string SelectedVehicleName { get; private set; }
+        public string SelectedVehicleName { get; private set; } = "BomberJetPlane";
+
+        private void Awake()
+        {
+            _initialRespawnText = _autoSpawnText.text;
+        }
+
+        public void Update()
+        {
+            if (_controlledVehicle == null || _controlledVehicle.IsDead)
+            {
+                if (!_autoSpawnText.enabled)
+                    _autoSpawnText.enabled = true;
+
+                _timeRemainingToAutoSpawn -= Time.deltaTime;
+                _autoSpawnText.text = _initialRespawnText + (int)_timeRemainingToAutoSpawn;
+
+                if (_timeRemainingToAutoSpawn <= 0 && !IsSpawnRequested)
+                    OnVehicleSelectionButtonClick(SelectedVehicleName);
+            }
+            else
+            {
+                if (_autoSpawnText.enabled)
+                    _autoSpawnText.enabled = false;
+
+                _timeRemainingToAutoSpawn = AutoSpawnTimeSeconds;
+            }
+
+            int health = _controlledVehicle?.HealthValue ?? 0;
+            _healthBarFill.fillAmount = (float)health / (_controlledVehicle?.MaxHealth ?? 100);
+            _healthText.text = health.ToString();
+        }
 
         public void ConfirmSpawn(IVehicle vehicle)
         {
@@ -27,13 +65,5 @@ namespace BananaParty.VehicleGame
             SelectedVehicleName = vehicleName;
             IsSpawnRequested = true;
         }
-
-        public void Update()
-        {
-            int health = _controlledVehicle?.HealthValue ?? 0;
-            _healthBarFill.fillAmount = (float)health / (_controlledVehicle?.MaxHealth ?? 100);
-            _healthText.text = health.ToString();
-        }
-
     }
 }
