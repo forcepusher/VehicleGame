f1ee7bf4efbff48ec4f4454196d816d360347e33
forcepusher <bionitsoup@gmail.com>
2026-06-27 00:30:52 +0700

    Work on weapons

---
+++ Assets/Source/Scripts/Weapons/Autocannon.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace BananaParty.VehicleGame
+{
+    public class Autocannon : MonoBehaviour
+    {
+        [SerializeField]
+        private Projectile _projectile;
+    }
+}
---
+++ Assets/Source/Scripts/Weapons/Projectile.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace BananaParty.VehicleGame
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField]
+        private float DirectHitDamage = 5;
+        [SerializeField]
+        private float ExplosionDamage = 5;
+        [SerializeField]
+        private float ExplosionRadius = 2;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            
+        }
+    }
+}
