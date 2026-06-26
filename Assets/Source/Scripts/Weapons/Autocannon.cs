662237fab2431c68c14f66c4a23b50a07978a39a
forcepusher <bionitsoup@gmail.com>
2026-06-27 02:18:13 +0700

    Add consistent fire rate firing

--- Assets/Source/Scripts/Weapons/Autocannon.cs
+++ Assets/Source/Scripts/Weapons/Autocannon.cs
@@ -10,15 +10,24 @@ namespace BananaParty.VehicleGame
         private Transform _projectileSpawnPoint;
         [SerializeField]
         private float _projectileSpeed = 50f;
+        [SerializeField]
         private float _fireRate = 5f;
+
+        private float _fireCooldown;
 
         private IControls _controls;
 
         private void Update()
         {
-            if (_controls.FirePrimary)
+            if (_fireCooldown > 0)
             {
+                _fireCooldown -= Time.deltaTime;
+            }
 
+            if (_controls.FirePrimary && _fireCooldown <= 0)
+            {
+                _fireCooldown = 1f / _fireRate;
+                Fire();
             }
         }
 
