7b4eea9df9ea44c9f2ce89b8c1e8e0d65fa06bb2
forcepusher <bionitsoup@gmail.com>
2026-06-29 03:17:51 +0700

    Fix camera effects

--- Assets/Source/Scripts/MainCameraEffects.cs
+++ Assets/Source/Scripts/MainCameraEffects.cs
@@ -13,20 +13,16 @@ namespace BananaParty.VehicleGame
         private void Update()
         {
             Vector3 followedObjectVelocity = Vector3.zero;
-            Quaternion followedObjectRotation = Quaternion.identity;
             if (_mainCamera.FollowTarget != null)
-            {
                 followedObjectVelocity = _mainCamera.FollowTarget.FollowVelocityGlobalSpace;
-                followedObjectRotation = _mainCamera.FollowTarget.FollowTransformFirstPerson.rotation;
-            }
 
-            SetVelocityFeelParticleSpeed(followedObjectVelocity, followedObjectRotation);
+            SetVelocityFeelParticleSpeed(followedObjectVelocity);
         }
 
-        private void SetVelocityFeelParticleSpeed(Vector3 speed, Quaternion rotation)
+        private void SetVelocityFeelParticleSpeed(Vector3 speed)
         {
             Vector3 worldParticleVelocity = -speed * 0.3f;
-            Vector3 particleVelocity = Quaternion.Inverse(rotation) * worldParticleVelocity;
+            Vector3 particleVelocity = Quaternion.Inverse(transform.rotation) * worldParticleVelocity;
             ParticleSystem.VelocityOverLifetimeModule velocityOverLifetime = _ashesParticleSystem.velocityOverLifetime;
             velocityOverLifetime.x = particleVelocity.x;
             velocityOverLifetime.y = particleVelocity.y;
